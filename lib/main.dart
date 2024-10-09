e5d89d6d5e9c2da56ef2ba33b5795e1b8107984b
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-09 23:37:26 +0300

    start app - add device preview - handel folders [mob/tab/desk]

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:portfolio/features/homepage.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Protofolio',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ProfileHomePage(),
+    );
+  }
+}
