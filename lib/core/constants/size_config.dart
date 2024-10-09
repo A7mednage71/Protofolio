a2f2f21a0a071514d8800aa1d5466b07fb02f41e
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-10 00:18:17 +0300

    create files for - DesktopLayout - TabletLayout - MobileLayout - create sizeConfig class to handel   screen width

---
+++ lib/core/constants/size_config.dart
@@ -0,0 +1,4 @@
+class SizeConfig {
+  static const double desktop = 950;
+  static const double tablet = 600;
+}
--- lib/features/homepage.dart
+++ lib/features/homepage.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/core/constants/size_config.dart';
+import 'package:portfolio/features/desktop_layout/desktop_layout.dart';
+import 'package:portfolio/features/mobile_layout/mobile_layout.dart';
+import 'package:portfolio/features/tablet_layout/tablet_layout.dart';
 
-class ProfileHomePage extends StatelessWidget {
-  const ProfileHomePage({super.key});
+class ProtofolioHomePage extends StatelessWidget {
+  const ProtofolioHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < SizeConfig.tablet) {
+          return const MobileLayout();
+        } else if (constraints.maxWidth < SizeConfig.desktop) {
+          return const TabletLayout();
+        } else {
+          return const DesktopLayout();
+        }
+      },
+    );
   }
 }
