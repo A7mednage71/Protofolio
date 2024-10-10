5d4fcfb9d826e98edbd606aaff0e224c2b14ce71
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-10 17:34:00 +0300

    create app styles class

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+
+class AppStyles {
+  static const BoxDecoration gradientBackground = BoxDecoration(
+    gradient: LinearGradient(
+      colors: [
+        AppColors.ebony,
+        AppColors.studio,
+      ],
+    ),
+  );
+}
