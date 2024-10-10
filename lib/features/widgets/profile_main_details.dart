2253d8f1037d8de96838c9d4cd66252d6684d54e
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-10 17:35:50 +0300

    create profile main details widget

---
+++ lib/features/widgets/profile_main_details.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class ProfileMainDetails extends StatelessWidget {
+  const ProfileMainDetails({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("I am Nageh",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 26,
+                fontWeight: FontWeight.bold)),
+        GradientText(
+          "Mobile App Developer +\nFlutter Developer",
+          colors: const [AppColors.studio, AppColors.paleSlate],
+          style: TextStyle(
+            fontSize: size.width * 0.035,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Poppins',
+          ),
+        ),
+        SizedBox(
+          height: size.height * 0.05,
+        ),
+        SizedBox(
+          width: size.width * 0.5,
+          child: const Text(
+            "Enthusiastic Junior Flutter Developer with a solid foundation in mobile\napp development and a passion for creating engaging user experiences and experienced\nin building cross-platform applications with Flutter framework.",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        )
+      ],
+    );
+  }
+}
