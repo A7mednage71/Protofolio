5ede5771381640db0371056bef72bf6f1b7017c8
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-14 22:48:24 +0300

    create custom widget for gradient text

---
+++ lib/features/widgets/gradient_custom_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/core/constants/size_config.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class GradientCustomText extends StatelessWidget {
+  const GradientCustomText({
+    super.key,
+    required this.size,
+    required this.title,
+  });
+
+  final Size size;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GradientText(
+      title,
+      colors: const [
+        AppColors.studio,
+        AppColors.paleSlate,
+      ],
+      style: TextStyle(
+        fontSize: size.width < SizeConfig.tablet
+            ? size.width * 0.05
+            : size.width * 0.025,
+        fontWeight: FontWeight.w600,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
