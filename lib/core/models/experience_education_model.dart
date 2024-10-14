25d0e2ea7d2c7409a8925e965adbdaf9eb6ddd72
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-14 22:47:54 +0300

    - create ExperienceOrEducationModel

---
+++ lib/core/models/experience_education_model.dart
@@ -0,0 +1,12 @@
+class ExperienceOrEducationModel {
+  final String title;
+  final String startyear;
+  final String endYear;
+  final String locationOrPosition;
+
+  ExperienceOrEducationModel(
+      {required this.title,
+      required this.startyear,
+      required this.endYear,
+      required this.locationOrPosition});
+}
