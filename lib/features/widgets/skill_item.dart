9550490f321158034185fe38b74fcbaf4127dc9f
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-15 02:36:49 +0300

    build skill item container

---
+++ lib/features/widgets/skill_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:portfolio/core/models/skill_model.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+
+class SkillItem extends StatefulWidget {
+  const SkillItem({super.key, required this.size, required this.skill});
+  final Size size;
+  final SkillModel skill;
+
+  @override
+  State<SkillItem> createState() => _SkillItemState();
+}
+
+class _SkillItemState extends State<SkillItem> {
+  bool isHover = false;
+  setHoverState(bool state) {
+    setState(() => isHover = state);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MouseRegion(
+          cursor: SystemMouseCursors.click,
+          onEnter: (event) => setHoverState(true),
+          onExit: (event) => setHoverState(false),
+          child: Container(
+              width: widget.size.width * 0.1,
+              height: widget.size.height * 0.2,
+              decoration: BoxDecoration(
+                color: AppColors.experienceItemBackground,
+                border: Border.all(
+                  color: isHover ? Colors.white : AppColors.studio,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: widget.size.height * 0.025),
+                  SvgPicture.asset(widget.skill.logo,
+                      height: widget.size.height * 0.1),
+                  const Spacer(),
+                  Text(widget.skill.percentage,
+                      style: TextStyle(
+                        color: isHover ? Colors.white : AppColors.studio,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  SizedBox(height: widget.size.height * 0.02),
+                ],
+              )),
+        ),
+        SizedBox(height: widget.size.height * 0.01),
+        Text(widget.skill.title,
+            style: TextStyle(
+              color: isHover ? Colors.white : AppColors.studio,
+              fontWeight: FontWeight.bold,
+            ))
+      ],
+    );
+  }
+}
