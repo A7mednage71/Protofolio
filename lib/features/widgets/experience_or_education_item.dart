f5914b0cadea624b02ea4f3848f3cb215077f101
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-14 22:48:03 +0300

    - build experience or education item

---
+++ lib/features/widgets/experience_or_education_item.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+
+class ExperienceOrEducationItem extends StatefulWidget {
+  const ExperienceOrEducationItem({
+    super.key,
+    required this.startYear,
+    required this.endYear,
+    required this.title,
+    required this.locationOrPosition,
+    required this.size,
+  });
+  final String startYear;
+  final String endYear;
+  final String title;
+  final String locationOrPosition;
+  final Size size;
+
+  @override
+  State<ExperienceOrEducationItem> createState() =>
+      _ExperienceOrEducationItemState();
+}
+
+class _ExperienceOrEducationItemState extends State<ExperienceOrEducationItem> {
+  bool isHovered = false;
+  sethoverState(bool bool) {
+    setState(() {
+      isHovered = bool;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (event) => sethoverState(true),
+      onExit: (event) => sethoverState(false),
+      child: Container(
+        width: widget.size.width * 0.4,
+        height: widget.size.height * 0.15,
+        decoration: BoxDecoration(
+          color: isHovered ? null : AppColors.experienceItemBackground,
+          gradient: isHovered
+              ? const LinearGradient(
+                  colors: [
+                    AppColors.studio,
+                    AppColors.valhalla,
+                  ],
+                )
+              : null,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${widget.startYear} - ${widget.endYear}",
+                style: TextStyle(
+                    color: isHovered ? Colors.white : AppColors.studio,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(widget.title,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+              Text(widget.locationOrPosition,
+                  style: const TextStyle(color: Colors.white)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
