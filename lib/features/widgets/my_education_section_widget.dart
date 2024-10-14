710159b5d67ef8c0cb3b4defd52de6e88b0a881e
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-14 22:54:28 +0300

    create MyExperienceSectionWidget && MyEducationSectionWidget in desktop layout

---
+++ lib/features/widgets/my_education_section_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:portfolio/core/constants/size_config.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:portfolio/features/widgets/experience_education_list_generate.dart';
+import 'package:portfolio/features/widgets/gradient_custom_text.dart';
+
+class MyEducationSectionWidget extends StatelessWidget {
+  const MyEducationSectionWidget({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Icon(
+              FontAwesomeIcons.school,
+              color: AppColors.studio,
+              size: size.width < SizeConfig.tablet
+                  ? size.width * 0.05
+                  : size.width * 0.025,
+            ),
+            const SizedBox(width: 30),
+            GradientCustomText(size: size, title: "My Education"),
+          ],
+        ),
+        SizedBox(height: size.height * 0.04),
+        EducationList(size: size),
+      ],
+    );
+  }
+}
---
+++ lib/features/widgets/my_experiene_section_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:portfolio/core/constants/size_config.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:portfolio/features/widgets/experience_education_list_generate.dart';
+import 'package:portfolio/features/widgets/gradient_custom_text.dart';
+
+class MyExperienceSectionWidget extends StatelessWidget {
+  const MyExperienceSectionWidget({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Icon(
+              FontAwesomeIcons.award,
+              color: AppColors.studio,
+              size: size.width < SizeConfig.tablet
+                  ? size.width * 0.05
+                  : size.width * 0.025,
+            ),
+            const SizedBox(width: 30),
+            GradientCustomText(size: size, title: "My Experience"),
+          ],
+        ),
+        SizedBox(height: size.height * 0.04),
+        ExperienceList(size: size),
+      ],
+    );
+  }
+}
