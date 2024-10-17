d1731ecee40acfdcc2a2edfefea433ff86c1938f
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-18 02:06:04 +0300

    handel drawer width

--- lib/features/mobile_layout/widgets/mobile_drawer.dart
+++ lib/features/mobile_layout/widgets/mobile_drawer.dart
@@ -10,6 +10,7 @@ class MobileDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      width: MediaQuery.of(context).size.width * 0.7,
       backgroundColor: AppColors.ebony,
       child: MobileDrawerList(sectionKeys: sectionKeys),
     );
